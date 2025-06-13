8ef5c10446fc0f32cb6fd8ec9d349fd074d0e245
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-13 19:16:02 +0200

    [milestone 3][completed]

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,13 @@
 // nationality: una stringa tra un insieme definito di valori.
 // Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
 
+// 📌 Milestone 3
+// Crea una funzione getActress che, dato un id, effettua una chiamata a:
+
+// GET /actresses/:id
+// La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+
+// Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta.
 
 type Person = {
   readonly id: number,
@@ -47,4 +54,40 @@ type Actress = Person & {
   most_famous_movies: [string, string, string],
   awards: string,
   nationality: Nationality
+}
+
+function isActress(dato: unknown): dato is Actress {
+  return (
+    typeof dato === 'object' && dato !== null &&
+    "id" in dato && typeof dato.id === 'number' &&
+    "name" in dato && typeof dato.name === 'string' &&
+    "birth_year" in dato && typeof dato.birth_year === 'number' &&
+    "death_year" in dato && typeof dato.death_year === 'number' &&
+    "biography" in dato && typeof dato.biography === 'string' &&
+    "image" in dato && typeof dato.image === 'string' &&
+    "most_famous_movies" in dato &&
+    dato.most_famous_movies instanceof Array &&
+    dato.most_famous_movies.length === 3 &&
+    dato.most_famous_movies.every(m => typeof m === 'string') &&
+    "awards" in dato && typeof dato.awards === 'string' &&
+    "nationality" in dato && typeof dato.nationality === 'string'
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const dato: unknown = await response.json();
+    if (!isActress(dato)) {
+      throw new Error('Formato dati non valido');
+    }
+    return dato;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Errore non riesco a recuperare l\'attrice:', error);
+    } else {
+      console.error('Errore sconosciuto:', error);
+    }
+    return null;
+  }
 }
