c6567ba746957584142904a235a4e449f0d98f75
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-13 18:43:55 +0200

    [milestone 2][completed]

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,15 @@
 // biography: breve biografia, stringa
 // image: URL dell'immagine, stringa
 
+// 📌 Milestone 2
+// Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+
+// most_famous_movies: una tuple di 3 stringhe
+// awards: una stringa
+// nationality: una stringa tra un insieme definito di valori.
+// Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
+
+
 type Person = {
   readonly id: number,
   readonly name: string,
@@ -17,4 +26,25 @@ type Person = {
   death_year?: number,
   biography: string,
   image: string
+}
+
+type Nationality =
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South"
+  | "African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South"
+  | "Korean"
+  | "Chinese"
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: Nationality
 }
